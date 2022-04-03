be0a08f7c7df206e31773051673d55e86682ea81
patri <patrickrichard@163.com>
2022-04-03 23:51:40 +0800

    Final code

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     devServer: {
         proxy: {
             '/api': {
-                target: 'https://my7e1b4mje.execute-api.us-east-1.amazonaws.com/gogoads',
+                target: 'https://my7e1b4mje.execute-api.us-east-1.amazonaws.com/gogoads/api',
                 changeOrigin: true,
                 ws: true,
                 secure: true,
