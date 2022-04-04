18f700ae0733483dac251163dee70e845755cf11
patri <patrickrichard@163.com>
2022-04-04 09:41:54 +0800

    Add API Auth

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
-import Amplify from "aws-amplify";
+import Amplify, { API } from "aws-amplify";
 import awsExports from "./aws-exports";
 import AmplifyVue from '@aws-amplify/ui-vue';
 import router  from './router/index'
@@ -11,6 +11,33 @@ import router  from './router/index'
 import axios from 'axios';
 
 Amplify.configure(awsExports);
+Amplify.configure({
+    // OPTIONAL - if your API requires authentication
+    Auth: {
+        // REQUIRED - Amazon Cognito Identity Pool ID
+        identityPoolId: 'us-east-1:1b97d481-77ea-4305-bcc5-2ccf7e2dced6',
+        // REQUIRED - Amazon Cognito Region
+        region: 'us-east-1',
+        // OPTIONAL - Amazon Cognito User Pool ID
+        userPoolId: 'us-east-1_ciXrJEGzU',
+        // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+        // userPoolWebClientId: 'a1b2c3d4e5f6g7h8i9j0k1l2m3',
+    },
+    API: {
+        endpoints: [
+            {
+                name: "gogoads",
+                endpoint: "https://my7e1b4mje.execute-api.us-east-1.amazonaws.com/gogoads"
+            },
+            // {
+            //     name: "MyCustomCloudFrontApi",
+            //     endpoint: "https://api.my-custom-cloudfront-domain.com",
+            //
+            // }
+        ]
+    }
+});
+
 const app = createApp(App);
 app.use(AmplifyVue);
 app.use(Antd)
